9b83594d4f9d966cecdcfecd237de2f76eab4dd6
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-03-03 21:32:49 +0200

    Added card at products form

--- oshop/src/app/admin/product-form/product-form.component.ts
+++ oshop/src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { ProductService } from './../../product.service';
 import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -10,12 +11,16 @@ import { Component, OnInit } from '@angular/core';
 export class ProductFormComponent implements OnInit {
   categories$;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) { 
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService, 
+    private productService: ProductService) { 
     this.categories$ = categoryService.getCategories();
   }
 
   save(product){
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
   }
 
   ngOnInit() {
