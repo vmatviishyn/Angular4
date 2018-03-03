233989a52953d5b4c96debf1f911598b499bb90e
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-03-03 10:56:23 +0200

    Added firebase

--- oshop/src/environments/environment.prod.ts
+++ oshop/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCorDBaz_zCOetMS_VePt4LuSjgCq_r18o",
+    authDomain: "oshop-caa30.firebaseapp.com",
+    databaseURL: "https://oshop-caa30.firebaseio.com",
+    projectId: "oshop-caa30",
+    storageBucket: "",
+    messagingSenderId: "856172547975"
+  }
 };
--- oshop/src/environments/environment.ts
+++ oshop/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCorDBaz_zCOetMS_VePt4LuSjgCq_r18o",
+    authDomain: "oshop-caa30.firebaseapp.com",
+    databaseURL: "https://oshop-caa30.firebaseio.com",
+    projectId: "oshop-caa30",
+    storageBucket: "",
+    messagingSenderId: "856172547975"
+  }
 };
