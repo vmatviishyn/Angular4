f4ce274ae14b5beb2ba230c0cb9851fa625fb5d1
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-03-03 20:34:06 +0200

    Product form added

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
+import { ProductFormComponent } from './admin/product-form/product-form.component';
 
 
 @NgModule({
@@ -37,7 +38,8 @@ import { LoginComponent } from './login/login.component';
     MyOrdersComponent,
     AdminProductsComponent,
     AdminOrdersComponent,
-    LoginComponent
+    LoginComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
@@ -58,6 +60,11 @@ import { LoginComponent } from './login/login.component';
       { 
         path: 'admin/products', 
         component: AdminProductsComponent, 
+        canActivate: [AuthGuard, AdminAuthGuard] 
+      },
+      { 
+        path: 'admin/products/new', 
+        component: ProductFormComponent, 
         canActivate: [AuthGuard, AdminAuthGuard] 
       },
       { 
