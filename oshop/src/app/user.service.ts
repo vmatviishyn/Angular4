4da3036414ceedd84adfa7256c60bd73f27a5369
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-03-03 18:42:32 +0200

    Protecting the Admin Routes

--- oshop/src/app/user.service.ts
+++ oshop/src/app/user.service.ts
@@ -1,5 +1,6 @@
+import { AppUser } from './models/app-user';
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import * as firebase from 'firebase';
 
 @Injectable()
@@ -12,6 +13,10 @@ export class UserService {
       name: user.displayName, 
       email: user.email
     });
+  }
+
+  get(uid: string): FirebaseObjectObservable<AppUser>{
+    return this.db.object('/users/' + uid);
   }
 
 }
