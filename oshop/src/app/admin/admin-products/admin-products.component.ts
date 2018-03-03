2c168dd6d6bd0b88f525d0ee7a8022aecbe85ece
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-03-03 22:18:53 +0200

    Added search of products

--- oshop/src/app/admin/admin-products/admin-products.component.ts
+++ oshop/src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,32 @@
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: {title: string}[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) { 
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll()
+    .subscribe(products => this.filteredProducts = this.products = products);
+  }
+
+  filter(query: string){
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())) : 
+      this.products;
   }
 
   ngOnInit() {
   }
 
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
