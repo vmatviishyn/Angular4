f8df025f38075a0d50d4a428b18d7f0232120e8d
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-03-03 13:57:59 +0200

    Extracting a service

---
+++ oshop/src/app/auth.service.ts
@@ -0,0 +1,22 @@
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase';
+
+@Injectable()
+export class AuthService {
+  user$: Observable<firebase.User>;
+
+  constructor(private afAuth: AngularFireAuth) {
+    this.user$ = afAuth.authState;
+  }
+
+  login(){
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout(){
+    this.afAuth.auth.signOut();
+  }
+
+}
