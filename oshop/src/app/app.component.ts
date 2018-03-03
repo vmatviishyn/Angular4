ed031c1b466c05a83220560ecc992ba48b036d23
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-03-03 23:45:15 +0200

    Fixed bug with redirecting

--- oshop/src/app/app.component.ts
+++ oshop/src/app/app.component.ts
@@ -11,13 +11,16 @@ import { Router } from '@angular/router';
 export class AppComponent {
   constructor(private userService: UserService, private auth: AuthService, router: Router){
     auth.user$.subscribe(user => {
-      if (user){
-        userService.save(user);
+      if (!user) return;
 
-        let returnUrl = localStorage.getItem('returnUrl');
-        router.navigateByUrl(returnUrl);
-      }
-    })
-  }
+      userService.save(user);
+
+      let returnUrl = localStorage.getItem('returnUrl');
+      if (!returnUrl) return;
+
+      localStorage.removeItem('returnUrl');
+      router.navigateByUrl(returnUrl);
+  })
+}
 }
 
