af653d4eb93108845db9c1f838f68bd0f277bb4c
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-03-04 11:58:39 +0200

    Created product filter component, refactoring code

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { AdminProductsComponent } from './admin/admin-products/admin-products.co
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
 import { ProductFormComponent } from './admin/product-form/product-form.component';
+import { ProductFilterComponent } from './products/product-filter/product-filter.component';
 
 
 @NgModule({
@@ -44,7 +45,8 @@ import { ProductFormComponent } from './admin/product-form/product-form.componen
     AdminProductsComponent,
     AdminOrdersComponent,
     LoginComponent,
-    ProductFormComponent
+    ProductFormComponent,
+    ProductFilterComponent
   ],
   imports: [
     BrowserModule,
--- oshop/src/app/products/products.component.ts
+++ oshop/src/app/products/products.component.ts
@@ -1,6 +1,5 @@
 import { Product } from './../models/product';
 import { ActivatedRoute } from '@angular/router';
-import { CategoryService } from './../category.service';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import 'rxjs/add/operator/switchMap';
@@ -13,13 +12,11 @@ import 'rxjs/add/operator/switchMap';
 export class ProductsComponent {
   products: Product[] = [];
   filteredProducts: Product[] = [];
-  categories$;
   category: string;
 
   constructor(
     route: ActivatedRoute,
-    productService: ProductService, 
-    categoryService: CategoryService) { 
+    productService: ProductService) { 
 
     productService
       .getAll()
@@ -34,11 +31,5 @@ export class ProductsComponent {
           this.products.filter(p => p.category === this.category) :
           this.products;
       });
-
-    this.categories$ = categoryService.getAll();
-
-
   }
-
-
 }
