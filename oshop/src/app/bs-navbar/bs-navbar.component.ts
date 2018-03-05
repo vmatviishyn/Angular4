78be661a76d76aeefe3b9ce9cbd8ee1edc5afdab
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-03-05 19:52:58 +0200

    Refactoring shopping cart count

--- oshop/src/app/bs-navbar/bs-navbar.component.ts
+++ oshop/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,7 +1,9 @@
+import { Observable } from 'rxjs/Observable';
 import { ShoppingCartService } from './../shopping-cart.service';
 import { AppUser } from './../models/app-user';
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Component({
   selector: 'bs-navbar',
@@ -11,7 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class BsNavbarComponent implements OnInit {
   public isCollapsed = true;
   appUser: AppUser;
-  shoppingCartItemCount: number;
+  cart$: Observable<ShoppingCart>;
 
   constructor(private auth: AuthService, private shoppingCartService: ShoppingCartService){
   }
@@ -19,12 +21,7 @@ export class BsNavbarComponent implements OnInit {
   async ngOnInit(){
     this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
 
-    let cart$ = await this.shoppingCartService.getCart();
-    cart$.subscribe(cart => {
-      this.shoppingCartItemCount = 0;
-      for (let productId in cart.items) 
-        this.shoppingCartItemCount += cart.items[productId].quantiti;
-    });
+    this.cart$ = await this.shoppingCartService.getCart();
   }
 
   toggleMenu() {
