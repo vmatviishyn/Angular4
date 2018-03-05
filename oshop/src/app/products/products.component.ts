89352a71318d0bea7286492b02a2edcbeba2eee2
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-03-05 17:11:16 +0200

    displaying the quantiti

--- oshop/src/app/products/products.component.ts
+++ oshop/src/app/products/products.component.ts
@@ -1,22 +1,28 @@
 import { Product } from './../models/product';
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../product.service';
 import 'rxjs/add/operator/switchMap';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
   products: Product[] = [];
   filteredProducts: Product[] = [];
   category: string;
+  cart: any;
+  subscription: Subscription;
 
   constructor(
     route: ActivatedRoute,
-    productService: ProductService) { 
+    productService: ProductService,
+    private shoppingCartService: ShoppingCartService
+  ) { 
 
     productService
       .getAll()
@@ -32,4 +38,14 @@ export class ProductsComponent {
           this.products;
       });
   }
+
+  async ngOnInit(){
+    this.subscription = (await this.shoppingCartService.getCart())
+      .subscribe(cart => this.cart = cart);
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
 }
